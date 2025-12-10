1e7955a3cccc32196ffd990ebd91c36fa582ac0e
Salla <sallatus.rouhiainen@gmail.com>
2025-12-10 18:01:46 +0200

    Rest of the toNumber tests

--- src/tests/toNumber.test.js
+++ src/tests/toNumber.test.js
@@ -11,14 +11,40 @@ describe("toNumber.js utility function", () => {
     expect(toNumber("1332")).toBe(1332);
   });
 
+
+      // testing if returns a number 
+  test("Returns a number", () => {
+    expect(toNumber(13)).toBe(13);
+  });
+
+
+        // testing if returns a number when the number is negative
+  test("Returns a negative", () => {
+    expect(toNumber(-13)).toBe(-13);
+  });
+
+
     test("Returns nan when is not numeral", () => {
-    expect(toNumber("%gA")).toBe(NaN);
+    expect(toNumber("%")).toBe(NaN);
   });
 
 
-      test("Returns nan if object", () => {
-    expect(toNumber("%gA")).toBe(NaN);
+   test("if object is a number, returns the same number", () => {
+    const obj = {
+      valueOf() { return 10; }
+    };
+    expect(toNumber(obj)).toBe(10);
   });
+
+     test("if object is not a number, returns NAN", () => {
+    const obj = {
+      valueOf() { return "gkgk"; }
+    };
+    expect(toNumber(obj)).toBe(NaN
+
+    );
+  });
+
 
 
 
